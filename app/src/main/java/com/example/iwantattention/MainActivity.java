479d5c9c569367f2dd119400a98b530e119f1b6e
Adam <adambrewer88@gmail.com>
2020-05-08 22:57:59 -0300

    updated SendMessage to send SMS instead of email, added RequestPermission method to get permission to use SMS

--- app/src/main/java/com/example/iwantattention/MainActivity.java
+++ app/src/main/java/com/example/iwantattention/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.iwantattention;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.PermissionRequest;
 import android.widget.Button;
 import com.google.android.material.snackbar.Snackbar;
+import android.telephony.SmsManager;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequestPermission();
         final Button btnAttention = findViewById(R.id.btnAttention);
         btnAttention.setBackgroundColor(Color.MAGENTA);
         final Button btnSnuggles = findViewById(R.id.btnSnuggles);
@@ -30,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         btnPrincess.setBackgroundColor(Color.MAGENTA);
         Button btnSend = findViewById(R.id.btnSend);
         btnSend.setBackgroundColor(Color.MAGENTA);
-        //string "message" will be the selected button as a string and will be sent in an email
+        //string "message" will be the selected button as a string and will be sent in a message
         //starts blank, and once a button is pushed, the message will be updated
         final String[] message = {""};
         //when a button is pressed, it changes colour to yellow,
@@ -120,15 +128,51 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String msg = message[0];
                 SendMessage(msg);
+                ShowNotification("Message sent!");
             }
         });
     }
+
+    private void RequestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        android.Manifest.permission.SEND_SMS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Send SMS permission");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please enable access to SMS.");
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {android.Manifest.permission.SEND_SMS}
+                                    , 1);
+                        }
+                    });
+                    builder.show();
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{android.Manifest.permission.SEND_SMS},
+                            1);
+                }
+            } else {
+               //
+            }
+        } else {
+            //
+        }
+    }
+
     public void ShowNotification(String message) {
         Snackbar display = Snackbar.make(findViewById(R.id.btnSend), message, Snackbar.LENGTH_LONG);
         display.show();
     }
     public void SendMessage(String message){
-        String[] TO = {"adambrewer88@gmail.com"};
+        //code to send over email - using SMS instead
+        /*String[] TO = {"adambrewer88@gmail.com"};
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.setData(Uri.parse("mailto:"));
         emailIntent.setType("message/rfc822");
@@ -140,6 +184,11 @@ public class MainActivity extends AppCompatActivity {
             finish();
         } catch (android.content.ActivityNotFoundException ex) {
             ShowNotification("There is no email client installed on this device");
-        }
+        }*/
+        SmsManager smsManager = SmsManager.getDefault();
+        String smsNumber = "15062605793";
+        String smsText = message + "\n\n sent from the iWantAttention app";
+        smsManager.sendTextMessage(smsNumber, null, smsText, null, null);
     }
 }
+
